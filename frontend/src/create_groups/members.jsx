81985e1dca1c51d5aa5f4cecdfc3f9019143e32c
Tojo07 <tsimalayh@berea.edu>
2023-06-26 11:38:20 -0400

    added option to rate each participant

--- frontend/src/create_groups/members.jsx
+++ frontend/src/create_groups/members.jsx
@@ -6,12 +6,16 @@ import {Send, getCookie} from '../components/send_data';
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { groupContext } from "../helper/group_context";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 const Members = () => {
+    const ratings = [1,2,3,4,5]
     const {setGroups} = useContext(groupContext)
     const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
     const [members, setMembers] = useState([])
     const [newMember, setNewMember] = useState('')
+    const [selected, setSelected] = useState(ratings[2])
 
     useEffect(()=>{
         const cached_members = sessionStorage.getItem('members')
@@ -34,29 +38,56 @@ const Members = () => {
                             <div className="relative bg-[#4169E1] w-full h-full">
                                 <div className="absolute flex flex-col place-items-center justify-center bg-white w-full h-full -left-[3%] -top-[3%] lg:justify-between lg:-left-[1.5%]">
                                     <div className="w-[100%] h-[80%] flex flex-col justify-center place-items-center lg:flex-row lg:justify-between">
-                                        <div className=" w-[70%] flex flex-col lg:w-[20%] lg:place-content-center lg:ml-4">
+                                        <div className=" w-[70%] flex flex-col lg:w-[35%] lg:place-content-center lg:ml-4">
                                             <div className=" text-gray-700 mb-2">
                                                 <label htmlFor="add">Add Participants</label>
                                             </div>
-                                            <input className="bg-[#E6F3FE] border-b-2 border-[#4169E1] text-gray-700 pl-2 lg:w-52 lg:h-11 outline-none" type="text" name="add" placeholder="participant's name..." value={newMember} 
-                                            onChange={(e) => setNewMember(e.target.value)}/>
+                                            <div className="h-12 flex flex-row">
+                                                <input className="w-[70%] bg-[#E6F3FE] border-b-2  border-[#4169E1] text-gray-700 pl-2 lg:h-12 outline-none" type="text" name="add" placeholder="participant's name..." value={newMember} 
+                                                onChange={(e) => setNewMember(e.target.value)}/>
+                                                <div>
+                                                    <button type="button" onClick={()=>{setIsOpen((prev)=>!prev)}} className="h-12 flex justify-between pl-2 pr-2 items-center bg-[#E6F3FE] border-b-2 border-[#4169E1]">
+                                                    {selected}
+                                                    {isOpen ? <IoMdArrowDropup className="text-2xl" /> : <IoMdArrowDropdown className="text-2xl" />}
+                                                    </button>
+                                                    {
+                                                        isOpen
+                                                        && 
+                                                        <div className="w-[100%] h-32 overflow-auto border-t-2  border-b-2 cursor-default border-[#4169E1] mb-4">
+                                                            <ul className="text-black flex flex-col items-center mb-2">
+                                                                {ratings.map((rating, i) => (
+                                                                    <button type="button" onClick={()=>{
+                                                                        setSelected(rating)
+                                                                        setIsOpen((prev)=>!prev)
+                                                                    }} key={i}  className="w-full h-12 bg-[#E6F3FE] border-b-2 border-[#4169E1] flex items-center justify-start pl-4 gap-2">
+                                                                        <li className="text-center ">{rating}</li>
+                                                                    </button>
+                                                                ))}
+                                                            </ul>
+                                                        </div>   
+                                                    }
+                                                </div>
+                                            </div>
                                             <button type='button' onClick={()=>{newMember.length > 0 && 
-                                                AddMember(newMember).then(()=>{
+                                                AddMember([newMember, selected]).then(()=>{
                                                     const json_array = JSON.stringify(members);
                                                     sessionStorage.setItem('members', json_array);
                                                     setNewMember('');
                                                 }).then( console.log(sessionStorage.getItem('members'))
-                                                )}} className=" bg-[#4169E1] text-center mb-2 lg:w-52 lg:h-11 text-white" >Add</button>
+                                                )}} className="w-[70%] bg-[#4169E1] text-center mb-2 h-11 text-white" >Add</button>
                                         </div>
                                         <div className=" w-[70%] max-h-[30%] overflow-auto cursor-default lg:mt-6 mb-4 lg:w-[60%] lg:max-h-[50%] lg:mr-4">
                                             {
                                                 members.length > 0 
                                                 &&
-                                                <ul className="text-black flex flex-col items-center mb-2">
+                                                <ul className="text-black flex flex-col  mb-2 justify-start">
                                                     {members.map((member, i) => (
-                                                        <div key={i} className="w-full h-12 bg-[#E6F3FE] border-b-2 border-[#4169E1] flex items-center justify-start pl-4 gap-2">
+                                                        <div key={i} className="w-[70%] h-12 bg-[#E6F3FE] border-b-2 border-[#4169E1] flex items-center justify-start pl-4 gap-2">
                                                             <FaRegUserCircle className="w-[24px] h-[24px] text-[#4169E1]"/>
-                                                            <li className="text-center ">{member}</li>
+                                                            <li className="w-full text-center flex flex-row">
+                                                                <h1 className="w-[50%] text-start">{member[0]}</h1>
+                                                                <h1 className="w-[20%]">{member[1]}</h1>
+                                                            </li>
                                                         </div>
                                                     ))}
                                                 </ul>
--- frontend/src/pages/maketeams.jsx
+++ frontend/src/pages/maketeams.jsx
@@ -1,15 +1,68 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { groupContext } from "../helper/group_context"
+import LoggedNav from "../components/navbar";
+import Footer from "../components/footer";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 const MakeTeams=()=> {
     const {groupName, setGroupName} = useContext(groupContext)
-
+    const options = ['random', 'skill-based', 'option 3', 'option 4']
+    const [isOpen, setIsOpen] = useState(false)
+    const [selected, setSelected] = useState(options[0])
     useEffect(()=>{
         const val = sessionStorage.getItem('groupName');
         setGroupName(val)
     }, []);
   return (
-    <div>{groupName != null && groupName}</div>
+    <>
+    <main className="h-screen">
+        <LoggedNav/>
+        <section className="relative bg-[#E6F3FE] min-h-screen">
+            <div className="absolute bg-[#4169E1] w-[60%] h-[70%] top-[16%] left-[22%]">
+                <div className="absolute flex flex-col place-items-center pt-6 gap-y-8 bg-white w-full h-full -top-[1%] -left-[2%] lg:-top-[2%] lg:-left-[1.5%]">
+                    <div className="">
+                        <h1>Activity Name</h1>
+                        <input className="bg-[#E6F3FE] text-gray-700 w-52 h-12 outline-none pl-2 border-b-2 border-[#4169E1] lg:w-64" type="text" />
+                    </div>
+                    <div className="">
+                        <h1>Team Size</h1>
+                        <input className="bg-[#E6F3FE] text-gray-700 w-52 h-12 outline-none pl-2 border-b-2 border-[#4169E1] lg:w-64" type="text" />
+                    </div>
+                    <div>
+                        <h1>Matching Options</h1>
+                        <button type="button" onClick={()=>{setIsOpen((prev)=>!prev)}} className="h-12 w-52 flex justify-between pl-2 pr-2 items-center bg-[#E6F3FE] border-b-2 border-[#4169E1] lg:w-64">
+                            {selected}
+                            {isOpen ? <IoMdArrowDropup className="text-2xl" /> : <IoMdArrowDropdown className="text-2xl" />}
+                        </button>
+                        {
+                            isOpen
+                                && 
+                            <div className="w-52 h-32 overflow-auto border-t-2 border-b-2 cursor-default border-[#4169E1] mb-4 lg:w-[313px] lg:h-[200px]">
+                                <ul className="text-black flex flex-col items-center mb-2">
+                                    {options.map((member, i) => (
+                                        <button type="button" onClick={()=>{
+                                            setSelected(member)
+                                            setIsOpen((prev)=>!prev)
+                                        }} key={i}  className="w-full h-12 bg-[#E6F3FE] border-b-2 border-[#4169E1] flex items-center justify-start pl-4 gap-2">
+                                            <FaRegUserCircle className="w-[24px] h-[24px] text-[#4169E1]"/>
+                                            <li className="text-center ">{member}</li>
+                                        </button>
+                                    ))}
+                                </ul>
+                            </div>   
+                        }
+                    </div>
+                </div>
+                <div>
+                    <h1>Matching Restrictions</h1>
+                    
+                </div>
+            </div>
+        </section>
+        <Footer/>
+    </main>
+    </>
   )
 }
 
-export default MakeTeams
+export default MakeTeams;
