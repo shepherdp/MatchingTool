ed49312daa7587886aa0723de6a0af3b0e7fa978
ayoubim <ayoubim@berea.edu>
2023-06-30 14:30:16 -0400

    Added the hovering effect to the buttons.

--- frontend/src/pages/team_options.jsx
+++ frontend/src/pages/team_options.jsx
@@ -1,39 +1,93 @@
 import LoggedNav from "../components/navbar";
 import Footer from "../components/footer";
-import {BiPlusMedical} from "react-icons/bi";
-import {AiFillEdit} from "react-icons/ai";
-import {GiPlayerPrevious} from "react-icons/gi";
-import react from 'react'
-const  TeamOptions=() =>{
-    return(
-        <>
-        <main className='w-screen h-screen bg-[#E6F3FE]'>
-            <LoggedNav />
-            <section className="bg-[#E6F3FE] h-screen">
-                <div className='relative w-[100%] h-[100%] bg-[#E6F3FE]'>
-                    <div className='flex justify-center items-center h-full gap-4'>
-                        <button type='button'>
-                            <div className='w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]'>
-                                <BiPlusMedical/>
-                            </div>
-                        </button>
-                        <button type='button'>
-                            <div className='w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]'>
-                                <AiFillEdit/>
-                                <span className='text-[#4169E1] text-[24px] mt-2 hover:text-[100px]'>Edit</span>
-                            </div>
-                        </button>
-                        <button type='button'>
-                            <div className='w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]'>
-                                <GiPlayerPrevious/>
-                            </div>
-                        </button>
-                    </div>
+import { BiPlusMedical } from "react-icons/bi";
+import { AiFillEdit } from "react-icons/ai";
+import { GiPlayerPrevious } from "react-icons/gi";
+import { useState } from 'react';
+
+const TeamOptions = () => {
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [isHovered3, setIsHovered3] = useState(false);
+
+  const handleMouseEnter1 = () => {
+    setIsHovered1(true);
+  };
+
+  const handleMouseLeave1 = () => {
+    setIsHovered1(false);
+  };
+
+  const handleMouseEnter2 = () => {
+    setIsHovered2(true);
+  };
+
+  const handleMouseLeave2 = () => {
+    setIsHovered2(false);
+  };
+
+  const handleMouseEnter3 = () => {
+    setIsHovered3(true);
+  };
+
+  const handleMouseLeave3 = () => {
+    setIsHovered3(false);
+  };
+
+  return (
+    <>
+      <main className='w-screen h-screen bg-[#E6F3FE]'>
+        <LoggedNav />
+        <section className="bg-[#E6F3FE] h-screen">
+          <div className='relative w-[100%] h-[100%] bg-[#E6F3FE]'>
+            <div className='flex justify-center items-center h-full gap-4'>
+              <button type='button'>
+                <div 
+                  className='w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]'
+                  onMouseEnter={handleMouseEnter1}
+                  onMouseLeave={handleMouseLeave1}
+                >
+                  {isHovered1 ? (
+                    <span className="text-[50px] font-bold overflow-hidden">Create groups</span>
+                  ) : (
+                    <BiPlusMedical />
+                  )}
                 </div>
-            </section>
-            <Footer />
-        </main>
-        </>
-    )
+              </button>
+              <button type="button">
+                <div
+                  className="w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]"
+                  onMouseEnter={handleMouseEnter2}
+                  onMouseLeave={handleMouseLeave2}
+                >
+                  {isHovered2 ? (
+                    <span className="text-[50px] font-bold overflow-hidden">Edit</span>
+                  ) : (
+                    <AiFillEdit />
+                  )}
+                </div>
+              </button>
+              <button type='button'>
+                <div
+                  className="w-[250px] h-[250px] bg-white border-4 border-[#4169E1] flex justify-center items-center text-[#4169E1] text-[90px] hover:text-[100px]"
+                  onMouseEnter={handleMouseEnter3}
+                  onMouseLeave={handleMouseLeave3}
+                >
+                  {isHovered3 ? (
+                    <span className="text-[50px] font-bold overflow-hidden">Previous Groups</span>
+                  ) : (
+                    <GiPlayerPrevious />
+                  )}
+                </div>
+              </button>
+            </div>
+          </div>
+        </section>
+        <Footer />
+      </main>
+    </>
+  );
 };
+
 export default TeamOptions;
+
