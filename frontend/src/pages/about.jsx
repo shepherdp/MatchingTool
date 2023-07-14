aa28169fc7110a4e0e9db8b1e6c1df6ee21c7316
Tojo07 <tsimalayh@berea.edu>
2023-07-14 15:24:33 -0400

    more change

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -48,17 +48,15 @@ const About =()=> {
                 </div> 
             </div>
             <div className='w-[40%] h-[45%] lg:w-[20%] lg:h-[80%] bg-white rounded-lg relative flex flex-col justify-center place-items-center shadow-lg shadow-[#4169E1] hover:scale-[101%]  ease-in-out'>
-                <div className=' w-28 h-28 lg:w-40 lg:h-40 border-4 border-[#4169E1] rounded-full overflow-hidden mt-2'>
+                <div className=' w-28 h-28 lg:w-36 lg:h-36 border-4 border-[#4169E1] rounded-full overflow-hidden mt-2'>
                     <img src={tojo} alt="#" className=' object-cover w-[140%] h-[140%]'/>
                 </div>
-                <div className='h-[60%] w-full text-gray-600 pl-2 flex flex-col gap-y-[3%] justify-normal pr-2 place-items-center text-center'>
+                <div className='h-[60%] w-full text-gray-600 pl-2 flex flex-col gap-y-[3%] justify-normal pr-2 place-items-center text-center overflow-x-auto'>
                     <h1 className='text-[110%] lg:text-[150%] font-bold'>Tojo H. Tsimalay</h1>
                     <h1 className='text-[90%] lg:text-[110%] font-semibold'>Computer science, Berea College</h1>
                     <p className=' font-light text-sm'>
                         I am a junior CS student at Berea College. I am mainly interested in data science and NLP, but
                         I recently grew interest in webapp development. I am the web developer of this project.
-                        Outside of classes, I like to work out, play and watch football (soccer), or hang out with 
-                        friends.
                     </p>   
                 </div>
                 <div className='w-full h-[10%] flex flex-row justify-center place-items-center gap-x-[5%] text-[150%] lg:text-[200%]'>
